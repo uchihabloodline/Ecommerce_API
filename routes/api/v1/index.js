5c813d6c03688dc8cbfc3938a7355fe62a180d95
shivam <geetapandey595@gmail.com>
2020-04-30 04:19:52 +0530

    routes modified

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/product', require('./products'));
+router.use('/product', require('./products'));
 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 
-router.get('/api',require('./api/index'));
+router.use('/api',require('./api/index'));
 
 
 module.exports = router;
